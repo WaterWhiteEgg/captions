5781998fb32a6d9a57677951d4af17da4911aa63
White Egg <2733259150@qq.com>
2023-11-12 23:07:00 +0800

    方法初始化

--- README.md
+++ README.md
@@ -1,19 +1,25 @@
 # captions
-
-## Project setup
+逆转裁判，简易字幕网页版
+## 下载依赖包
 ```
 npm install
 ```
 
-### Compiles and hot-reloads for development
+### 运行网页（本地测试）
 ```
 npm run serve
 ```
 
-### Compiles and minifies for production
-```
-npm run build
-```
+### Tips
+由于浏览器同源策略，媒体文件不可以直接应用于网页，如果需要使用assets的音频则需要将其放置在8080端口的本地服务器上面
+_为什么要8080呢，因为我就写了8080，如果你优先 npm run serve 导致http-server端口是8081之类觉对会报错的。。。_
+ - 使用 npm 全局安装 http-server：
+
+ `npm install -g http-server`
+ 
+- 切换到你的音频文件所在的目录，然后运行 http-server：
+
+ `http-server`
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,128 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="box">
+    <div v-if="currentIndex < subtitles.length" :key="currentIndex">
+      {{ currentSubtitle }}
+    </div>
+    <button @click="nextSubtitle" v-if="showButton">下一个</button>
+  </div>
 </template>
 
+<script setup>
+import { ref, watch, onMounted } from 'vue';
+// 索引值
+const currentIndex = ref(0);
+// 文本
+const subtitles = ["", "第一句字幕第一句字 幕第 一句'fff'句 字幕第一句字幕第一句字幕第一句字幕，第一句字幕", "第二句字幕", "第三句字幕"]; // 字幕数组
+// 活跃的要显示的文本
+const currentSubtitle = ref(subtitles[currentIndex.value]);
+// 控制点击按钮显示
+const showButton = ref(true);
+
+// 下一个活跃文本
+const nextSubtitle = () => {
+  // 超过文本最大值，索引不再增加
+  if (currentIndex.value < subtitles.length - 1) {
+    currentIndex.value++;
+    // 触发点击音效
+    playSound("http://localhost:8080/点击查看.mp3")
+    showButton.value = false; // 隐藏按钮
+  }
+};
+
+// 开启并使用音频
+function playSound(src) {
+  const audio = new Audio(src); // 替换成你的音效文件路径
+  audio.play();
+}
+
+
+// 节流阀
+function throttle(func, delay) {
+  let timer = null;
+  return function () {
+    if (!timer) {
+      func.apply(this, arguments);
+      timer = setTimeout(() => {
+        timer = null;
+      }, delay);
+    }
+  };
+}
+// 观察索引值的变化，使其切换活跃显示文本
+watch(currentIndex, (index) => {
+  // 切换下一个字幕
+  currentSubtitle.value = subtitles[currentIndex.value];
+  // 一字一字显示
+  renderSubtitle()
+
+
+});
+// 创建标点符号的正则表达式
+const punctuationRegex = /[。,，\s]/;
+
+// 更新定时器的时间延迟
+function updateIntervalDelay(test) {
+  return punctuationRegex.test(test) ? 200 : 60;
+}
+// 一字一字显示,且遇到punctuationRegex匹配的情况时变化
+function renderSubtitle() {
+  // 判断长度是否到头了
+  if (currentIndex.value < subtitles.length) {
+    // 清空当前字幕内容,防止显示所有字体
+    currentSubtitle.value = '';
+    // 当前要修改的字幕
+    let subtitle = subtitles[currentIndex.value];
+    // 新的索引值
+    let index = 0;
+    // 一个定时器
+    let interval;
+    // 更新定时器的时间延迟
+    updateIntervalDelay(subtitle.charAt(index - 1))
+    // 让currentSubtitle活跃显示字幕的被subtitle赋值,从而当subtitle截取一个个字时动态显示
+    function displayNextCharacter() {
+      currentSubtitle.value = subtitle.substring(0, index);
+      index++;
+      // 当索引值比字幕长度还小时,继续执行方法,否则停止定时器并显示按钮
+      if (index <= subtitle.length) {
+        // 更新定时器的时间延迟,currentIntervalDelay是返回值
+        let currentIntervalDelay = updateIntervalDelay(subtitle.charAt(index - 1));
+        // 清除当前定时器，为了重新提供新的定时器的值
+        clearInterval(interval);
+        // 重新创建一个定时器，这里其实算递归，没有 index <= subtitle.length 的话会一直执行 
+        interval = setTimeout(displayNextCharacter, currentIntervalDelay); // 根据新的时间间隔创建新的定时器
+        // 同时播放音频
+        playSound("http://localhost:8080/1_字幕弹出音.mp3")
+
+      } else {
+        // 显示按钮
+        showButton.value = true;
+      }
+    }
+
+    // 别忘了这里的函数不是displayNextCharacter而是renderSubtitle，要使用还要调用它
+    displayNextCharacter();
+  }
+}
+</script>
+
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+/* 样式部分保持不变 */
+.subtitle {
+  height: 20px;
+  overflow: hidden;
+  animation: showSubtitle 6.3s steps(30, end) forwards;
+  /* 3秒显示完整句子 */
 }
 
-nav {
-  padding: 30px;
-}
+@keyframes showSubtitle {
+  from {
+    width: 0;
+  }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+  to {
+    width: 100%;
+  }
 }
 </style>
+
